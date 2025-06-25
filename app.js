599c9a8b5cfc829e308f9ee69c35259df0844aa6
ksenyay <ksenyausachova@gmail.com>
2025-06-25 10:56:54 +0300

    Changed border-radius

--- app.js
+++ app.js
@@ -20,7 +20,10 @@ const app = express();
 
 // Connect to MongoDB database
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("connected to db"))
   .catch((err) => console.log(err));
 
